b82b9348fd206d264e4d518dae00e79d84c77f6b
wendell otim <wotim@csquared.com>
2024-04-11 12:55:26 +0300

    add the checkifpair method

--- src/memory.js
+++ src/memory.js
@@ -17,7 +17,17 @@ class MemoryGame {
 
   checkIfPair(card1, card2) {
     // ... write your code here
+      this.pairsClicked++;
+  
+      if (card1 === card2) {
+        this.pairsGuessed++;
+        return true; 
+      } else {
+        return false; 
+      }
+    }
   }
+
 
   checkIfFinished() {
     // ... write your code here
